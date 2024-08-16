67a755af996832f9c50c2c8e507737077c30e909
etesong014 <etesong0114@gmail.com>
2024-08-16 20:29:57 +0900

    api를 활용해 현재 위치 날씨정보 가져오기

---
+++ weather-app/src/App.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+
+// 1. 앱이 실행되자마자 현재위반 기반의 날씨가 보인다.
+// 2. 날씨정보에는 도시, 섭씨, 화씨 날씨상태가 있다.
+// 3. 5개의 버튼이 있다.(1개는 현재위치, 4개는 다른 도시)
+// 4. 도시버튼을 클릭할때 마다 도시별 날씨가 나온다.
+// 5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나온다.
+// 6. 데이터를 들고오는 동안 로딩 스피너가 돈다.
+function App() {
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position)=>{
+      let lat = position.coords.latitude
+      let lon =position.coords.longitude
+      getWeatherByCurrentLocation(lat, lon)
+    });
+  }
+
+  const getWeatherByCurrentLocation = async(lat, lon) => {
+    const KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY}`
+    let response = await fetch(url)
+    let data = await response.json()
+    console.log(data)
+  }
+
+  useEffect(()=> {
+    getCurrentLocation()
+  },[])
+  return <div>
+    hi
+  </div>;
+}
+
+export default App;
